e8fe3a3b8272f782ee373a56a52f217b16fe229c
Alisa Parashchenko <ge24cuc@mytum.de>
2024-02-10 15:56:30 +0100

    Make importEK work even if there's trailing whitespace

--- python_client/importEK.py
+++ python_client/importEK.py
@@ -1,7 +1,7 @@
 from Crypto.Hash import SHA256
 
 str = input("Copypaste key here:")
-data = bytes([ int(b, 16) for b in str.split(" ") ])
+data = bytes([ int(b, 16) for b in str.strip().split(" ") ])
 hash_object = SHA256.new(data)
 f = open("EK_hash.txt", "w")
 f.write(hash_object.hexdigest())
